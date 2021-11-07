e021f0c82e84634ec06eae4f3b611d88f8f5db56
Chris Hutcherson <cjh190001@utdallas.edu>
2021-11-06 19:13:06 -0500

    initi the employeeInfoMap

--- GUIModel.java
+++ GUIModel.java
@@ -1,13 +1,17 @@
+import java.io.IOException;
 import java.sql.*;
+import java.util.HashMap;
 
 public class GUIModel {
     private LoginController loginController;
     private DBManager dbManager;
+    private HashMap<String, HashMap<String, String>> employeeInfoMap;
 
 
-    public GUIModel() {
+    public GUIModel() throws IOException {
         loginController = new LoginController();
         dbManager = new DBManager();
+        HashMap<String, HashMap<String, String>> employeeInfoMap = dbManager.getEmployeeInfoMap();
     }
     public LoginController getLoginController() {
         return loginController;
@@ -23,5 +27,9 @@ public class GUIModel {
 
     public void setDbManager(DBManager dbManager) {
         this.dbManager = dbManager;
+    }
+
+    public HashMap<String, HashMap<String, String>> getEmployeeInfoMap() {
+        return employeeInfoMap;
     }
 }
