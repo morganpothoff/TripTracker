57af248a58825d22966c8be1c5986d75d3b33180
Chris Hutcherson <cjh190001@utdallas.edu>
2021-11-11 20:22:17 -0600

    added comments

--- GUIApplication.java
+++ GUIApplication.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 public class GUIApplication {
     public static void main(String[] args) throws IOException {
         // Assemble all the pieces of the MVC
+        // run program
         new GUIController(new GUIModel(), new GUIView("LoginPage")).initController();
     }
 }
