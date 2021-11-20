ae1c2e081295e8cd27eb60024b928deb26d5b882
Tanamori <93846823+Tanamori@users.noreply.github.com>
2021-11-19 19:55:23 -0600

    Update Users.java
    Added inserts to database for changing password and email.

--- Users.java
+++ Users.java
@@ -103,6 +103,9 @@ public class Users {
         boolean retVal = false;
         if(currentPassword.equals(password)) {
             password = newPassword;
+            String get_user_query = String.format("UPDATE `Users` SET `Password` = '%s', WHERE 'User_ID' = '%d';", newPassword, getUserID());
+            ConnectedDBConnection connection = new ConnectedDBConnection();
+            ResultSet user_results = connection.select(get_user_query);
             retVal = true;
         }
         return retVal;
@@ -112,6 +115,9 @@ public class Users {
         boolean retVal = false;
         if(currentEmail.equals(email)) {
             email = newEmail;
+            String get_user_query = String.format("UPDATE `Users` SET `eMail` = '%s', WHERE 'User_ID' = '%d';", newEmail, getUserID());
+            ConnectedDBConnection connection = new ConnectedDBConnection();
+            ResultSet user_results = connection.select(get_user_query);
             retVal = true;
         }
         return retVal;
